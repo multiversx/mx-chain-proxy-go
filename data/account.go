3baba9fbc3fc4ad234bc8208bb8a6c8dc50b44b7
ccorcoveanu <cristi.corcoveanu@gmail.com>
2019-07-30 13:54:59 +0300

    Add code field to account response

--- data/account.go
+++ data/account.go
@@ -5,6 +5,7 @@ type Account struct {
 	Address  string `json:"address"`
 	Nonce    uint64 `json:"nonce"`
 	Balance  string `json:"balance"`
+	Code     string `json:"code"`
 	CodeHash []byte `json:"codeHash"`
 	RootHash []byte `json:"rootHash"`
 }
