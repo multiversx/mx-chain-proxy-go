490b9ea9aae01f72cbf389ee8b04dde373b38179
iulianpascalau <iulian.pascalau@gmail.com>
2019-10-25 10:56:51 +0300

    changed tx.value from big.Int to string

--- data/transaction.go
+++ data/transaction.go
@@ -4,15 +4,15 @@ import "math/big"
 
 // Transaction represents the structure that maps and validates user input for publishing a new transaction
 type Transaction struct {
-	Nonce     uint64   `form:"nonce" json:"nonce"`
-	Value     *big.Int `form:"value" json:"value"`
-	Receiver  string   `form:"receiver" json:"receiver"`
-	Sender    string   `form:"sender" json:"sender"`
-	GasPrice  uint64   `form:"gasPrice" json:"gasPrice,omitempty"`
-	GasLimit  uint64   `form:"gasLimit" json:"gasLimit,omitempty"`
-	Data      string   `form:"data" json:"data,omitempty"`
-	Signature string   `form:"signature" json:"signature,omitempty"`
-	Challenge string   `form:"challenge" json:"challenge,omitempty"`
+	Nonce     uint64 `form:"nonce" json:"nonce"`
+	Value     string `form:"value" json:"value"`
+	Receiver  string `form:"receiver" json:"receiver"`
+	Sender    string `form:"sender" json:"sender"`
+	GasPrice  uint64 `form:"gasPrice" json:"gasPrice,omitempty"`
+	GasLimit  uint64 `form:"gasLimit" json:"gasLimit,omitempty"`
+	Data      string `form:"data" json:"data,omitempty"`
+	Signature string `form:"signature" json:"signature,omitempty"`
+	Challenge string `form:"challenge" json:"challenge,omitempty"`
 }
 
 // ResponseTransaction defines a response tx holding the resulting hash
