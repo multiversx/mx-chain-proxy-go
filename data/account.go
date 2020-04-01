dfae493967c617cb88c4c69b7463132d1e97fe51
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2020-04-01 15:32:48 +0300

    change order

--- data/account.go
+++ data/account.go
@@ -12,12 +12,12 @@ type Account struct {
 
 // ValidatorApiResponse represents the data which is fetched from each validator for returning it in API call
 type ValidatorApiResponse struct {
+	TempRating               float32 `json:"tempRating"`
 	NumLeaderSuccess         uint32  `json:"numLeaderSuccess"`
 	NumLeaderFailure         uint32  `json:"numLeaderFailure"`
 	NumValidatorSuccess      uint32  `json:"numValidatorSuccess"`
 	NumValidatorFailure      uint32  `json:"numValidatorFailure"`
 	Rating                   float32 `json:"rating"`
-	TempRating               float32 `json:"tempRating"`
 	TotalNumLeaderSuccess    uint32  `json:"totalNumLeaderSuccess"`
 	TotalNumLeaderFailure    uint32  `json:"totalNumLeaderFailure"`
 	TotalNumValidatorSuccess uint32  `json:"totalNumValidatorSuccess"`
