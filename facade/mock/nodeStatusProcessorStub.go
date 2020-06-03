2aaec895cfc8b6a227818fa322671e774c1c65c6
Iuga Mihai <mihai9625@gmail.com>
2020-06-03 13:05:11 +0300

    remove api route node/status/:shardID

--- facade/mock/nodeStatusProcessorStub.go
+++ facade/mock/nodeStatusProcessorStub.go
@@ -2,7 +2,6 @@ package mock
 
 // NodeStatusProcessorStub --
 type NodeStatusProcessorStub struct {
-	GetShardStatusCalled    func(shardID uint32) (map[string]interface{}, error)
 	GetEpochMetricsCalled   func(shardID uint32) (map[string]interface{}, error)
 	GetConfigMetricsCalled  func() (map[string]interface{}, error)
 	GetNetworkMetricsCalled func(shardID uint32) (map[string]interface{}, error)
@@ -21,9 +20,4 @@ func (nsps *NodeStatusProcessorStub) GetNetworkStatusMetrics(shardID uint32) (ma
 // GetEpochMetrics --
 func (nsps *NodeStatusProcessorStub) GetEpochMetrics(shardID uint32) (map[string]interface{}, error) {
 	return nsps.GetEpochMetricsCalled(shardID)
-}
-
-// GetShardStatus --
-func (nsps *NodeStatusProcessorStub) GetShardStatus(shardID uint32) (map[string]interface{}, error) {
-	return nsps.GetShardStatusCalled(shardID)
 }
