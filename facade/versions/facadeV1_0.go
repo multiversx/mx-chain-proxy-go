cb3818263b21db1e128339f2b62ec0a71402e881
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2020-10-12 20:05:43 +0300

    EN-7804: removed unused files

--- facade/versions/facadeV1_0.go
+++
@@ -1,8 +0,0 @@
-package versions
-
-import "github.com/ElrondNetwork/elrond-proxy-go/facade"
-
-// FacadeV1_0 represents the facade handler for the version v1.0
-type FacadeV1_0 struct {
-	*facade.ElrondProxyFacade
-}
--- facade/versions/facadeV1_1.go
+++
@@ -1,8 +0,0 @@
-package versions
-
-import "github.com/ElrondNetwork/elrond-proxy-go/facade"
-
-// FacadeV1_1 represents the facade handler for the version v1.1
-type FacadeV1_1 struct {
-	*facade.ElrondProxyFacade
-}
