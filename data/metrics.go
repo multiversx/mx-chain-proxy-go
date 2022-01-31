b9e3d4c7ee9417af6597aa6981220273b3c37bfe
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2022-01-31 13:12:59 +0200

    add comment

--- data/metrics.go
+++ data/metrics.go
@@ -2,6 +2,7 @@ package data
 
 import "time"
 
+// EndpointMetrics holds statistics about the requests for a specific endpoint
 type EndpointMetrics struct {
 	NumRequests         uint64        `json:"num_requests"`
 	NumErrors           uint64        `json:"num_errors"`
