bb6f12e86a86b99caf3df21711875beddbafeaf1
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-06-09 16:48:27 +0300

    Fix blockInfo serialization.

--- data/blockInfo.go
+++ data/blockInfo.go
@@ -3,6 +3,6 @@ package data
 // BlockInfo defines the data structure for the block at which an resource (e.g. Account object) is fetched from the Network
 type BlockInfo struct {
 	Nonce    uint64 `json:"nonce"`
-	Hash     []byte `json:"hash"`
-	RootHash []byte `json:"rootHash"`
+	Hash     string `json:"hash"`
+	RootHash string `json:"rootHash"`
 }
