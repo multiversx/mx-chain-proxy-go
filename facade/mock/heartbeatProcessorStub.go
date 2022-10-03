7732f9e011f1f7fee1427b4e7ab6345ee77cdcae
Bogdan Rosianu <bogdan.rosianu@yahoo.com>
2022-10-03 21:49:18 +0300

    fix tests

--- facade/mock/heartbeatProcessorStub.go
+++ facade/mock/heartbeatProcessorStub.go
@@ -4,7 +4,13 @@ import "github.com/ElrondNetwork/elrond-proxy-go/data"
 
 // HeartbeatProcessorStub represents a stub implementation of a HeartbeatProcessor
 type HeartbeatProcessorStub struct {
-	GetHeartbeatDataCalled func() (*data.HeartbeatResponse, error)
+	GetHeartbeatDataCalled     func() (*data.HeartbeatResponse, error)
+	IsOldStorageForTokenCalled func(tokenID string, nonce uint64) (bool, error)
+}
+
+// IsOldStorageForToken -
+func (hbps *HeartbeatProcessorStub) IsOldStorageForToken(tokenID string, nonce uint64) (bool, error) {
+	return hbps.IsOldStorageForTokenCalled(tokenID, nonce)
 }
 
 // GetHeartbeatData will call the handler func
