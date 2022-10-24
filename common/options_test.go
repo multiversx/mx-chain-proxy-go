52ed69c02aecf85d29a45ad2d9ccb9ac34749074
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-10-24 17:40:00 +0300

    Add extra unit tests.

--- common/options_test.go
+++ common/options_test.go
@@ -1,23 +1,65 @@
 package common
 
 import (
+	"net/url"
 	"testing"
 
+	"github.com/ElrondNetwork/elrond-go-core/core"
 	"github.com/stretchr/testify/require"
 )
 
 func TestBuildUrlWithBlockQueryOptions_ShouldWork(t *testing.T) {
-	url := BuildUrlWithBlockQueryOptions("/block/by-nonce/15", BlockQueryOptions{})
-	require.Equal(t, "/block/by-nonce/15", url)
+	builtUrl := BuildUrlWithBlockQueryOptions("/block/by-nonce/15", BlockQueryOptions{})
+	require.Equal(t, "/block/by-nonce/15", builtUrl)
 
-	url = BuildUrlWithBlockQueryOptions("/block/by-nonce/15", BlockQueryOptions{
+	builtUrl = BuildUrlWithBlockQueryOptions("/block/by-nonce/15", BlockQueryOptions{
 		WithTransactions: true,
 	})
-	require.Equal(t, "/block/by-nonce/15?withTxs=true", url)
+	require.Equal(t, "/block/by-nonce/15?withTxs=true", builtUrl)
 
-	url = BuildUrlWithBlockQueryOptions("/block/by-nonce/15", BlockQueryOptions{
+	builtUrl = BuildUrlWithBlockQueryOptions("/block/by-nonce/15", BlockQueryOptions{
 		WithTransactions: true,
 		WithLogs:         true,
 	})
-	require.True(t, url == "/block/by-nonce/15?withTxs=true&withLogs=true" || url == "/block/by-nonce/15?withLogs=true&withTxs=true")
+	parsed, err := url.Parse(builtUrl)
+	require.Nil(t, err)
+	require.Equal(t, "/block/by-nonce/15", parsed.Path)
+	require.Equal(t, "true", parsed.Query().Get("withTxs"))
+	require.Equal(t, "true", parsed.Query().Get("withLogs"))
+}
+
+func TestBuildUrlWithAccountQueryOptions_ShouldWork(t *testing.T) {
+	builtUrl := BuildUrlWithAccountQueryOptions("/address/erd1alice", AccountQueryOptions{})
+	require.Equal(t, "/address/erd1alice", builtUrl)
+
+	builtUrl = BuildUrlWithAccountQueryOptions("/address/erd1alice", AccountQueryOptions{
+		BlockNonce: core.OptionalUint64{HasValue: true, Value: 42},
+	})
+	require.Equal(t, "/address/erd1alice?blockNonce=42", builtUrl)
+
+	builtUrl = BuildUrlWithAccountQueryOptions("/address/erd1alice", AccountQueryOptions{
+		BlockHash: "abba",
+	})
+	require.Equal(t, "/address/erd1alice?blockHash=abba", builtUrl)
+
+	// The following isn't a valid scenario in the real world, according to the validation defined in:
+	// https://github.com/ElrondNetwork/elrond-go/blob/master/api/groups/addressGroupOptions.go
+	// However, here, we are testing each code path.
+	builtUrl = BuildUrlWithAccountQueryOptions("/address/erd1alice", AccountQueryOptions{
+		OnFinalBlock:   true,
+		OnStartOfEpoch: core.OptionalUint32{HasValue: true, Value: 1},
+		BlockNonce:     core.OptionalUint64{HasValue: true, Value: 2},
+		BlockHash:      "aabb",
+		BlockRootHash:  "bbaa",
+		HintEpoch:      core.OptionalUint32{HasValue: true, Value: 3},
+	})
+	parsed, err := url.Parse(builtUrl)
+	require.Nil(t, err)
+	require.Equal(t, "/address/erd1alice", parsed.Path)
+	require.Equal(t, "true", parsed.Query().Get("onFinalBlock"))
+	require.Equal(t, "1", parsed.Query().Get("onStartOfEpoch"))
+	require.Equal(t, "2", parsed.Query().Get("blockNonce"))
+	require.Equal(t, "aabb", parsed.Query().Get("blockHash"))
+	require.Equal(t, "bbaa", parsed.Query().Get("blockRootHash"))
+	require.Equal(t, "3", parsed.Query().Get("hintEpoch"))
 }
